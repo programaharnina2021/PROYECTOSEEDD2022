8a58c952e68cc43cafc80d07e4f2436b4ba4c168
programacion <>
2022-02-15 09:56:11 +0100

    Update MasterMind5Test.java

--- Testing Ejemplos/src/mastermind/MasterMind5Test.java
+++ Testing Ejemplos/src/mastermind/MasterMind5Test.java
@@ -2,14 +2,17 @@ package mastermind;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 import mastermind.dominio.ColorCombinacion;
 
 class MasterMind5Test {
 
-	@Testz
+	@Test
 	void testSortearEnigma() {
+		boolean contains = Arrays.asList(ColorCombinacion.values()).contains("verde");
 		//comndicion inicial
 		MasterMind5 masterMind5=new MasterMind5();
 		int expected=4;
@@ -17,6 +20,9 @@ class MasterMind5Test {
 		//primera prueba longitud correcta
 		assertEquals(expected, masterMind5.getEnigma().size());
 		for (int i = 0; i < expected; i++) {
+			boolean containsTeo = Arrays
+					.asList(ColorCombinacion.values())
+					.contains(masterMind5.getEnigma().getAt(i));
 			assertTrue(
 					ColorCombinacion.isValid(
 							masterMind5.getEnigma().getAt(i)));
